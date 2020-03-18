01124e76582f2fdc99707a328bf51e723af76d29
caoyao <caoyao@testbird.com>
2020-03-18 14:28:12 +0800

    d

--- app/index.js
+++ app/index.js
@@ -2,6 +2,19 @@ var express = require("express");
 var app = express();
 var gedtClientIp = require("./get_ip").gedtClientIp;
 var des = require("./encode").des
+app.all("*",function(req,res,next){
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    //允许的header类型
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    // res.header("Access-Control-Allow-Headers","content-type");
+    //跨域允许的请求方式 
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() == 'options')
+        res.send(200);  //让options尝试请求快速结束
+    else
+        next();
+});
 app.get('/backend/api/get_file_pass_token', (req, res) => {
     var ip = gedtClientIp(req).split("ffff:")[1];
     console.log(66666);
@@ -20,17 +33,5 @@ app.get('/backend/api/get_file_pass_token', (req, res) => {
 })
 app.use(express.static('./public'));
 
-app.all("*",function(req,res,next){
-    //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin","*");
-    //允许的header类型
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
-    // res.header("Access-Control-Allow-Headers","content-type");
-    //跨域允许的请求方式 
-    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-    if (req.method.toLowerCase() == 'options')
-        res.send(200);  //让options尝试请求快速结束
-    else
-        next();
-})
+
 app.listen(63342);
