3933f5d1f4f88219f6922e3459ed60a224bdf812
caoyao <caoyao@testbird.com>
2020-03-18 11:22:47 +0800

    d

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
+var gedtClientIp = require("./get_ip").gedtClientIp;
 var des = require("./encode").des
-app.get('/get_pass_token', (req, res) => {
+app.get('/get_file_pass_token', (req, res) => {
     console.log(req)
+    var ip = gedtClientIp(req);
+    console.log(99999999)
+    console.log(ip)
     var encrypt_text = des.encrypt('1583313594157$$64.64.234.45$$123$$456$$567$$789');
     console.log(66666);
     console.log(encrypt_text);
